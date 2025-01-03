69bfc18fe72167310beba2e9527999ada709785b
ljh5960 <wkwlqkfqkf10@gmail.com>
2025-01-03 15:47:39 +0900

    [GP-LJH] 메세지 detail : 상세변경사항

--- gamepiece/src/main/java/gamepiece/admin/game/service/GameListServiceImple.java
+++ gamepiece/src/main/java/gamepiece/admin/game/service/GameListServiceImple.java
@@ -1,6 +1,7 @@
 package gamepiece.admin.game.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import gamepiece.admin.game.mapper.GameListMapper;
 @Service
 public class GameListServiceImple implements GameListService{
 	private final GameListMapper gameListMapper;
+	
 	
 	public GameListServiceImple(GameListMapper gameListMapper) {
 		this.gameListMapper = gameListMapper;
@@ -23,8 +25,20 @@ public class GameListServiceImple implements GameListService{
 	
 	@Override
 	public Game getGameInfoByGameCode(String gameCode) {
-		
 		return gameListMapper.getGameInfoByCode(gameCode);
 	}
+	
+	@Override
+	public void modifyGame(Game game) {
+		gameListMapper.modifyGame(game);
+		
+	}
+	
+	@Override
+	public List<Map<String, Object>> getPlatform() {
+		// TODO Auto-generated method stub
+		return gameListMapper.getPlatform();
+	}
+	
 	
 }
