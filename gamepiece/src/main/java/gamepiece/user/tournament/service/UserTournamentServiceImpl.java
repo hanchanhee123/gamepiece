066753534d52ca399c4e6ba1c9959d0879b89d55
LeeJiseongs <ekflal159@naver.com>
2025-01-07 23:02:05 +0900

    [GP-LJS] 사용자 대회 컨트롤러 및 서비스 이름 변경 detail : 사용자 화면 대회 컨트롤러, 서비스, 매퍼 클래스 및 인터페이스명 변경

---
+++ gamepiece/src/main/java/gamepiece/user/tournament/service/UserTournamentServiceImpl.java
@@ -0,0 +1,8 @@
+package gamepiece.user.tournament.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserTournamentServiceImpl implements UserTournamentService{
+
+}
