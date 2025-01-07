ccceb54c50e4596c3f0faa84c278fcfa17753315
LeeJiseongs <ekflal159@naver.com>
2025-01-07 18:01:30 +0900

    [GP-LJS] 사용자 대회 화면 리스트 설정 detail : 사용자 대회화면 설정

---
+++ gamepiece/src/main/java/gamepiece/user/tournament/mapper/TournamentMapper.java
@@ -0,0 +1,10 @@
+package gamepiece.user.tournament.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component("userTournamentMapper")
+@Mapper
+public interface TournamentMapper {
+
+}
---
+++ gamepiece/src/main/java/gamepiece/user/tournament/service/TournamentServiceImpl.java
@@ -0,0 +1,8 @@
+package gamepiece.user.tournament.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("userTournamentService")
+public class TournamentServiceImpl implements TournamentService{
+	
+}
