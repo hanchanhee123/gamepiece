90844c3c4d704c6aa6e4d6ccec242bea89d6bac8
HongJeongHo <hmannayou12@naver.com>
2025-01-08 16:51:32 +0900

    [GP-이니셜] 메세지 detail : 게시판덧글(추가,수정,삭제), 카테고리(추가,수정,삭제)

--- gamepiece/src/main/java/gamepiece/admin/board/controller/BoardController.java
+++ gamepiece/src/main/java/gamepiece/admin/board/controller/BoardController.java
@@ -28,34 +28,29 @@ public class BoardController {
 	public String removeBoard(@RequestParam("boardNum") String boardNum, RedirectAttributes rttr) {
 	    int result = boardService.removeBoard(boardNum);
 
-	    if(result > 0) {
+	  
 	        rttr.addFlashAttribute("message", "게시글이 삭제되었습니다.");
-	    } else {
-	        rttr.addFlashAttribute("error", "게시글 삭제에 실패했습니다.");
-	    }
+	   
 	    return "redirect:/admin/board/list";
 	}
-	
-	
-	
-	
 	
 	
 	@PostMapping("/modify")
 	public String modifyBoard(Board board, RedirectAttributes rttr) {
 	    int result = boardService.modifyBoard(board);
 	    
-	    if(result > 0) {
+	   
 	        rttr.addFlashAttribute("message", "게시글이 수정되었습니다.");
 	      
-	    } 
+	  
 	    return "redirect:/admin/board/list";
 	}
+	
 	@GetMapping("/modify")
 	public String modifyBoardView(@RequestParam(name="boardNum") String boardNum, Pageable pageable, Model model) {
 		
 
-		List<BoardCategory> categoryList = (List<BoardCategory>) boardCategoryService.getBoardCategoryList(pageable);
+		List<BoardCategory> categoryList = boardCategoryService.getBoardCategoryList();
 		Board boardInfo = boardService.getBoardInfo(boardNum);
 		
 		model.addAttribute("title","게시글수정");
@@ -83,7 +78,7 @@ public class BoardController {
 	public String addBoardView(Pageable pageable,Model model) {
 		
 		
-		List<BoardCategory> categoryList = (List<BoardCategory>) boardCategoryService.getBoardCategoryList(pageable);
+		List<BoardCategory> categoryList = boardCategoryService.getBoardCategoryList();
 		
 		model.addAttribute("title", "게시물작성");
 
--- gamepiece/src/main/java/gamepiece/admin/boardComment/service/BoardCommentService.java
+++ gamepiece/src/main/java/gamepiece/admin/boardComment/service/BoardCommentService.java
@@ -1,21 +1,17 @@
-package gamepiece.admin.boardComment.mapper;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
+package gamepiece.admin.boardComment.service;
 
 import gamepiece.admin.boardComment.domain.BoardComment;
+import gamepiece.util.PageInfo;
 import gamepiece.util.Pageable;
 
-@Mapper
-public interface BoardCommentMapper {
+public interface BoardCommentService {
+	
 	
 	//덧글삭제
-	int removeComment(String commentNum);	
-
+		int removeComment(String commentNum);	
+	
 	//덧글수정
 	int modifyComment(BoardComment boardComment);
-	
 	//특정행 조회
 	BoardComment getCommentInfo(String commnetNum);
 	
@@ -23,10 +19,8 @@ public interface BoardCommentMapper {
 	//덧글작성
 	int addComment(BoardComment boardComment);
 	
-	//덧글세기
-	int getCntComment();
 	
 	//덧글목록
-		List<BoardComment> getCommentList(Pageable pageable);
+	PageInfo<BoardComment> getCommentList(Pageable pageable);
 
 }
