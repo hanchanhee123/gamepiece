0e2831adc25dcd9b69c2314ed7250b41897a7f2d
hanchanhee123 <cldkdk352@naver.com>
2025-01-13 17:35:00 +0900

    [GP-HCH] 포인트샵 인서트 detail : 포인트샵 인서트

---
+++ gamepiece/src/main/java/gamepiece/user/pointShop/domain/PointShopLog.java
@@ -0,0 +1,12 @@
+package gamepiece.user.pointShop.domain;
+
+import lombok.Data;
+
+@Data
+public class PointShopLog {
+	private String pslNo;
+	private String id;
+	private String psCd;
+	private int usedPoint;
+	private String logYmd;
+}
