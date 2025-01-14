affeb97dfcb800e65065c3766a428f3b14c5be78
anue0120 <joeuni7279@gmail.com>
2025-01-14 17:30:25 +0900

    [GP-JEA] myPage 아바타, 아바타액자 detail : 아바타, 아바타액자 모달 수정

---
+++ gamepiece/src/main/java/gamepiece/user/user/domain/AvatarFrame.java
@@ -0,0 +1,16 @@
+package gamepiece.user.user.domain;
+
+import lombok.Data;
+
+@Data
+public class AvatarFrame {
+	
+	private String avatarframeNo;
+	private String id;
+	private String psCd;
+	private String avatarframeYn;
+	private String regYmd;
+	private String psNm;
+	private String imgUrl;
+
+}
